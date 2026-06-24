5ce85db770272aa781a158e6fde7e37d22e65eb2
Omi0077 <omshankar2016@gmail.com>
2026-06-24 23:13:28 +0530

    created concurrent linked list header file

---
+++ LinkedList/LinkedList.h
@@ -0,0 +1,69 @@
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+
+struct Node{
+  int data{};
+  Node *next{nullptr}; // since we using malloc these default values wont be initialized
+};
+
+struct LinkedList{
+  Node *head{nullptr};
+  pthread_mutex_t gLock{};
+
+  void init(){
+    this->head = nullptr;
+    pthread_mutex_init(&(this->gLock), NULL);
+  }
+
+  void insert(int data){
+    Node* temp = static_cast<Node*>(malloc(sizeof(Node)));
+    if(temp == nullptr){
+      perror("malloc");
+      std::exit(1);
+    }
+    temp->data = data;
+
+    pthread_mutex_lock(&(this->gLock));
+    temp->next = this->head;
+    this->head = temp;
+    pthread_mutex_unlock(&(this->gLock));
+  }
+
+  bool contains(int target){
+    bool rv {false};
+
+    pthread_mutex_lock(&(this->gLock));
+    Node* temp {this->head};
+    while(temp){
+      if(temp->data == target){
+        rv = true;
+        break;
+      }
+      temp = temp->next;
+    }
+    pthread_mutex_unlock(&(this->gLock));
+
+    return rv;
+  }
+
+};
+
+inline std::ostream& operator << (std::ostream &out, LinkedList& l){
+    pthread_mutex_lock(&(l.gLock));
+    Node *temp{l.head};
+    while(temp){
+      out << temp->data << " ";
+      temp = temp->next;
+    }
+    pthread_mutex_unlock(&(l.gLock));
+
+    return out;
+  }
+
+
+
+#endif
---
+++ LinkedList/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <pthread.h>
+
+#include "LinkedList.h"
+
+
+void* threadRoutine(void* arg){
+  LinkedList *l = static_cast<LinkedList*>(arg);
+  for(int i=0; i<1e2; i++){
+    l->insert(i);
+  }
+
+  return nullptr;
+}
+
+int main(){
+
+  LinkedList l1{};
+  l1.init();
+  // l1.insert(5);
+  // l1.insert(4);
+  // l1.insert(3);
+  // l1.insert(2);
+  // l1.insert(1);
+
+  // std::cout << std::boolalpha << l1.contains(4) << "\n";
+
+  pthread_t t1, t2;
+
+  pthread_create(&t1, NULL, threadRoutine, &l1);
+  pthread_create(&t2, NULL, threadRoutine, &l1);
+
+  pthread_join(t1, NULL);
+  pthread_join(t2, NULL);
+
+  std::cout << l1 << "\n";
+
+
+  return 0;
+}
